cd7ba77aac95ab7a21b668e9650607d5b79f5a55
Joao miguel Correia rito <joacorre@c1r4s5.42lisboa.com>
2022-03-13 18:36:34 +0000

    c

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: joacorre <joacorre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/28 23:54:51 by joacorre          #+#    #+#             */
-/*   Updated: 2022/03/13 13:10:32 by joacorre         ###   ########.fr       */
+/*   Updated: 2022/03/13 18:30:36 by joacorre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,8 +27,15 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char			*substr;
 	unsigned int	i;
 
-	substr = malloc((len + 1) * sizeof(char));
-	if (!substr || ft_strlen(s) < start)
+	if (start > ft_strlen(s))
+	{
+		substr = malloc(sizeof(char));
+		substr[0] = '\0';
+		return (substr);
+	}
+	else
+		substr = malloc((len + 1) * sizeof(char));
+	if (!substr)
 		return (NULL);
 	i = 0;
 	while (i < len && s[start] != '\0')
@@ -44,8 +51,8 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 /*
 int main()
 {
-	char a[] = "abcdef";
-	char *ptr = ft_substr(a, 2, 1);
+	char a[] = "lorem ipsum dolor sit amet";
+	char *ptr = ft_substr(a, 400, 20);
 	printf("%s", ptr);
 }
 */
