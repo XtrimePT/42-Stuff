6ecb1ca6ee71ef2dcdd79ada1b5d5d46561749a6
BuildTools <unconfigured@null.spigotmc.org>
2022-04-27 00:58:56 +0100

    c

---
+++ ft_striteri.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joacorre <joacorre@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/27 00:34:03 by joacorre          #+#    #+#             */
+/*   Updated: 2022/04/27 00:34:03 by joacorre         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*void    ft(unsigned int i, char* s)
+{
+    s[i]++;
+}*/
+
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
+{
+	unsigned int i;
+	
+	i = 0;
+	while (s[i] != '\0')
+	{
+	    f(i, s);
+	    i++;
+	}
+}
+
+/*int main()
+{
+    char a[] = "abcd";
+    void (*f)(unsigned int, char*);
+    f = &ft;
+    ft_striteri(a, f);
+    printf("%s", a);
+    return (0);
+}*/
