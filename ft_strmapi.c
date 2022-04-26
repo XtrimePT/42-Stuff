0d9ad8322e60b971da50c555ffb3ef224ff883bc
BuildTools <unconfigured@null.spigotmc.org>
2022-04-27 00:33:46 +0100

    c

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -12,12 +12,36 @@
 
 #include "libft.h"
 
-char	*f(unsigned int a, char c)
+/*char    ft(unsigned int a, char c)
 {
-
-}
+    a = a;
+    return (c);
+}*/
 
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-
+    unsigned int    i;
+    char            *ptr;
+    
+    i = 0;
+    if (!(ptr = malloc(ft_strlen(s) + 1 * sizeof(char))))
+        return (NULL);
+    while (s[i] != '\0')
+    {
+        ptr[i] = f(i, s[i]);
+        i++;
+    }
+    ptr[i] = '\0';
+    return (ptr);
 }
+
+/*int main()
+{
+    char a[] = "abcd";
+    char (*f)(unsigned int, char);
+    f = &ft;
+    char *ptr = ft_strmapi(a, f);
+    printf("%s", ptr);
+    
+    return (0);
+}*/
