9a1dcba3ece53327e9f4beee49e8416abd0e1f26
Joao miguel Correia rito <joacorre@c1r20s13.42lisboa.com>
2022-06-23 15:24:03 +0100

    c

--- ft_memchr.c
+++ ft_memchr.c
@@ -14,12 +14,12 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t	i;
-	char	*ptr;
+	size_t			i;
+	unsigned char	*ptr;
 
-	ptr = (char *)s;
+	ptr = (unsigned char *)s;
 	i = 0;
-	while (ptr[i] != '\0' && i < n)
+	while (n--)
 	{
 		if (ptr[i] == c)
 			return (ptr + i);
